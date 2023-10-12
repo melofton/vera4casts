3bf71918776709d4d421e7b254b85a5d5803c9ea
Mary Lofton <melofton@vt.edu>
2023-10-12 15:15:49 -0400

    final prep for automation I hope

--- run_workflow.R
+++ run_workflow.R
@@ -1,8 +1,10 @@
-# Install R packages
+# Workflow script
 # Author: Mary Lofton
 # Date: 12OCT23
 
-# Purpose: install R packages that aren't already in neon4cast rocker
+# Purpose: run forecasting workflow for VERA
+
+# install R packages that aren't already in neon4cast rocker
 install.packages("remotes")
 install.packages("tidyverse")
 install.packages("lubridate")
@@ -12,3 +14,8 @@ install.packages("feasts")
 install.packages("urca")
 library(remotes)
 remotes::install_github("LTREB-reservoirs/vera4castHelpers", force = TRUE)
+
+source("./code/workflow_scripts/01_format.data.R")
+source("./code/workflow_scripts/03_predict.R")
+
+
