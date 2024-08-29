64923823f213d03d731a76ea2e96c357446d2683
Mary Lofton <melofton@vt.edu>
2024-08-29 13:39:54 -0400

    hopefully fixing workflow problem

--- run_retro_workflow.R
+++ run_retro_workflow.R
@@ -21,41 +21,51 @@ library(aws.s3)
 
 # check for any missing forecasts
 message("==== Checking for missed forecasts ====")
-challenge_model_name <- 'fableARIMA'
+challenge_model_names <- c('fableARIMA','fableETS','fableNNETAR')
 
 # Dates of forecasts 
 today <- paste(Sys.Date() - days(2), '00:00:00')
-this_year <- data.frame(date = as.character(paste0(seq.Date(as_date('2023-10-01'), to = as_date(today), by = 'day'), ' 00:00:00')),
-                        exists = NA)
-
+dates = as.character(paste0(seq.Date(as_date('2023-10-01'), to = as_date(today), by = 'day'), ' 00:00:00'))
+this_year <- matrix(data = NA, nrow = length(challenge_model_names), ncol = length(dates)+1)
+this_year <- data.frame(this_year)
+colnames(this_year) <- c("model_id",dates)
+this_year$model_id <- challenge_model_names
+  
 # vera bucket
 s3 <- arrow::s3_bucket("bio230121-bucket01/vera4cast/inventory/catalog",
                        endpoint_override = "renc.osn.xsede.org",
                        anonymous = TRUE)
 
-# submitted_forecasts_df <- arrow::open_dataset(s3) |> collect()
-
-content_raw <- readr::read_file_raw(s3)
-content_buffer <- buffer(content_raw)
-submitted_forecasts_df <- read_parquet(content_buffer)
+submitted_forecasts_df <- arrow::open_dataset(s3) |> 
+  filter(model_id %in% challenge_model_names) |>
+  collect()
 
 # is that forecast present in the bucket?
-for (i in 1:nrow(this_year)) {
+for (i in 2:ncol(this_year)) {
+  
+  for(j in 1:length(challenge_model_names)){
   
   models_submitted <- unique(submitted_forecasts_df %>%
-    filter(reference_date == this_year$date[i]) %>%
+    filter(reference_date == dates[i]) %>%
     pull(model_id))
 
-  this_year$exists[i] <- ifelse(challenge_model_name %in% models_submitted,T,F)
+  this_year[j,i] <- ifelse(challenge_model_names[j] %in% models_submitted,T,F)
+  
+  }
   
 }
 
+# wrangle back to long format
+this_year_long <- this_year %>%
+  pivot_longer(!model_id, names_to = "date", values_to = "exists")
+
 # which dates do you need to generate forecasts for?
 # those that are missing or haven't been submitted
-missed_dates <- this_year |> 
+missed_dates <- this_year_long |> 
   filter(!(exists == T)) |> 
   pull(date) |> 
-  as_date()
+  as_date() |>
+  unique()
 
 if (length(missed_dates) != 0) {
   for (i in 1:length(missed_dates)) {
