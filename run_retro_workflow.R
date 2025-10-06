d3d46c5d3d2ac6b62d63c8669201f06546f9e8ae
melofton <melofton@vt.edu>
2025-10-06 13:23:25 -0400

    further edits

--- run_retro_workflow.R
+++ run_retro_workflow.R
@@ -26,82 +26,67 @@ challenge_model_names <- c('fableARIMA','fableETS','fableNNETAR')
 # Dates of forecasts 
 today <- paste(Sys.Date() - days(2), '00:00:00')
 dates = as.character(paste0(seq.Date(as_date('2025-01-01'), to = as_date(today), by = 'day'), ' 00:00:00'))
+this_year_dates <- date(dates)
 this_year <- matrix(data = NA, nrow = length(challenge_model_names), ncol = length(dates)+1)
 this_year <- data.frame(this_year)
 colnames(this_year) <- c("model_id",dates)
 this_year$model_id <- challenge_model_names
-  
-# # vera bucket
-# s3 <- arrow::s3_bucket("bio230121-bucket01/vera4cast/inventory/catalog",
-#                        endpoint_override = "amnh1.osn.mghpcc.org",
-#                        anonymous = TRUE)
-# 
-# submitted_forecasts_df <- arrow::open_dataset(s3) |> 
-#   filter(model_id %in% challenge_model_names) |>
-#   collect()
-# 
-# # is that forecast present in the bucket?
-# for (i in 2:ncol(this_year)) {
-#   
-#   for(j in 1:length(challenge_model_names)){
-#   
-#   models_submitted <- unique(submitted_forecasts_df %>%
-#     filter(reference_date == dates[i]) %>%
-#     pull(model_id))
-# 
-#   this_year[j,i] <- ifelse(challenge_model_names[j] %in% models_submitted,T,F)
-#   
-#   }
-#   
-# }
-# 
-# # wrangle back to long format
-# this_year_long <- this_year %>%
-#   pivot_longer(!model_id, names_to = "date", values_to = "exists")
-# 
-# # which dates do you need to generate forecasts for?
-# # those that are missing or haven't been submitted
-# missed_dates <- this_year_long |> 
-#   filter(!(exists == T)) |> 
-#   pull(date) |> 
-#   as_date() |>
-#   unique()
 
-for (m in challenge_model_names){
+# is that forecast present in the bucket?
+
   
-  s3_model_path <- paste0('bio230121-bucket01/vera4cast/forecasts/archive-parquet/project_id=vera4cast/duration=P1D/variable=Chla_ugL_mean/model_id=',m)
-  
-  s3 <- arrow::s3_bucket(bucket = glue::glue(s3_model_path),
-                         endpoint_override = "https://amnh1.osn.mghpcc.org",
-                         anonymous = TRUE)
-  
-  avail_dates <- gsub("reference_date=", "", s3$ls())
-  
-  this_year$exists <- ifelse(as.Date(this_year$date) %in% as.Date(avail_dates), T, F)
-  
-  rerun_dates <- this_year |> filter(exists == FALSE) |> pull(date)
-  
-  
-  if (length(rerun_dates) != 0) {
-    for (i in 1:length(rerun_dates)) {
-      
-      curr_reference_datetime <- rerun_dates[i]
-      
-      message(paste("creating forecasts for",print(curr_reference_datetime)))
-      
-      # download the noaa once then apply the forecasts
-      source("./code/workflow_scripts/01_format_data.R")
-      message('data formatted!')
-      
-      # Script to run forecasts
-      source("./code/workflow_scripts/03_predict.R")
-      message('forecasts submitted!')
-      
-    }
-  } else {
-    message('no missed forecasts')  
+for(j in 1:length(challenge_model_names)){
+    
+    # vera bucket
+    s3_model_path <- paste0('bio230121-bucket01/vera4cast/forecasts/archive-parquet/project_id=vera4cast/duration=P1D/variable=Chla_ugL_mean/model_id=',challenge_model_names[j])
+    s3 <- arrow::s3_bucket(glue::glue(s3_model_path),
+                           endpoint_override = "amnh1.osn.mghpcc.org/",
+                           anonymous = TRUE)
+    
+    submitted_forecasts_df <- arrow::open_dataset(s3) |> 
+      filter(reference_date %in% this_year_dates) |>
+      collect()
+    
+    dates_submitted <- unique(submitted_forecasts_df %>%
+                                 pull(reference_date))
+    
+    for (i in 2:ncol(this_year)) {
+    
+    this_year[j,i] <- ifelse(this_year_dates[i] %in% dates_submitted,T,F)
+    
   }
+  
+}
 
-} # end of model loop
+# wrangle back to long format
+this_year_long <- this_year %>%
+  pivot_longer(!model_id, names_to = "date", values_to = "exists")
 
+# which dates do you need to generate forecasts for?
+# those that are missing or haven't been submitted
+missed_dates <- this_year_long |> 
+  filter(!(exists == T)) |> 
+  pull(date) |> 
+  as_date() |>
+  unique()
+
+if (length(missed_dates) != 0) {
+  for (i in 1:length(missed_dates)) {
+    
+    curr_reference_datetime <- missed_dates[i]
+    
+    message(paste("creating forecasts for",print(curr_reference_datetime)))
+    
+    # download the noaa once then apply the forecasts
+    source("./code/workflow_scripts/01_format_data.R")
+    message('data formatted!')
+    
+    # Script to run forecasts
+    source("./code/workflow_scripts/03_predict.R")
+    message('forecasts submitted!')
+    
+  }
+} else {
+  message('no missed forecasts')  
+}
 
